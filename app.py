5ffcedce755d017775a600dd9eb154e05920cde2
loulunds <lousanne.lundstrom@gmail.com>
2020-09-20 01:31:35 +0000

    requirements.txt, Procfile and templates added

--- app.py
+++ app.py
@@ -1,11 +1,57 @@
 import os
-from flask import Flask
+from os import path
+if path.exists("env.py"):
+    import env
+from flask import Flask, redirect, request, render_template, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'recipeDB'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html',
+        recipes=mongo.db.recipes.find(),
+        products=mongo.db.products.find())
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add-recipe.html')
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes=mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    return render_template('edit-recipe.html', recipe=the_recipe)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes=mongo.db.recipes
+    recipes.update({'_id': ObjectId(recipe_id)},
+    {
+        'recipe_title': request.form.get('recipe_title'),
+        'contributor_name': request.form.get('contributor_name'),
+        'ingredients': request.form.get('ingredients'),
+        'procedure': request.form.get('procedure'),
+        'image_url': request.form.get('image_url')
+    })
+    return redirect(url_for('get_recipes'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
