db234c21444e079ff690496ca42b74156b5c04bb
loulunds <lousanne.lundstrom@gmail.com>
2020-09-30 18:24:15 +0000

    whole-recipe.html fixed

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,8 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $("a[href='#top']").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 700, 'swing',);
+        return false;
+    });
  })
